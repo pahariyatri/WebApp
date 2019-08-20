a760ec927cdfdc11dec5198be5250d95fc39e9b8
pankaj <jaswalp090@gmail.com>
2019-08-21 00:50:11 +0530

    Add all my files

--- PahariYatri/settings.py
+++ PahariYatri/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIRS = 'static'
+STATICFILES_DIRS = [
+    STATIC_DIRS,
+]
--- PahariYatri/views.py
+++ PahariYatri/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
-
+from  .models import  Destination
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.price = 7000
+    dest1.img = 'top_1.jpg'
+
+    dest2 = Destination()
+    dest2.name = 'hyderabad'
+    dest2.price = 8000
+    dest2.img = 'top_2.jpg'
+
+    dest3 = Destination()
+    dest3.name = 'Bengaluru'
+    dest3.price = 7050
+    dest3.img = 'top_3.jpg'
+
+    dest4 = Destination()
+    dest4.name = 'Delhi'
+    dest4.price = 10000
+    dest4.img = 'top_4.jpg'
+
+    dests = [dest1, dest2, dest3, dest4]
+
+    return render(request, 'index.html', {'dests':dests})
